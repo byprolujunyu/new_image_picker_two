46e29b30cd48a6ef7b6fb38cb60f3729ce681a8a
lujunyu <toplu1015@163.com>
2019-11-08 10:55:03 +0800

    picker

--- lib/picker.dart
+++ lib/picker.dart
@@ -1,6 +1,17 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
+
+const String kTypeImage = 'image';
+const String kTypeVideo = 'video';
+
+/// Specifies the source where the picked image should come from.
+enum ImageSource {
+  /// Opens up the device camera, letting the user to take a new picture.
+  camera,
+
+  /// Opens the user's photo gallery.
+  gallery,
+}
 
 class MultiImagePicker {
   static const MethodChannel _channel =
@@ -10,5 +21,5 @@ class MultiImagePicker {
     return _channel.invokeMethod('getGallery');
   }
 
-  
+
 }
