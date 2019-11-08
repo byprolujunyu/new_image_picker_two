4422cd5f8b7f6f0eb0d1ffd6a3a4d9f0123bff9e
lujunyu <toplu1015@163.com>
2019-11-08 10:33:26 +0800

    ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,15 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-
+#include "ImagePickerPlugin.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    
+    [ImagePickerPlugin  registerWithRegistrar:[self registrarForPlugin:@"plugins.flutter.io/image_picker"]
+       ];  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
---
+++ ios/imagepicker/ImagePickerPlugin.m
@@ -0,0 +1,89 @@
+//
+//  ImagePickerPlugin.m
+//  Runner
+//
+//  Created by 鲁隽彧 on 2019/10/29.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+#import "ImagePickerPlugin.h"
+@interface ImagePickerPlugin ()<TZImagePickerControllerDelegate>
+@property (strong, nonatomic) FlutterResult result;
+@end
+@implementation ImagePickerPlugin
+{
+    
+}
+UIViewController *_viewController;
+- (instancetype)initWithViewController:(UIViewController *)viewController {
+  self = [super init];
+  if (self) {
+    _viewController = viewController;
+  }
+  return self;
+}
+
++ (void)registerWithRegistrar:(NSObject <FlutterPluginRegistrar> *)registrar {
+    FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"plugins.flutter.io/image_picker" binaryMessenger:[registrar messenger] ];
+    UIViewController *viewController =
+         [UIApplication sharedApplication].delegate.window.rootViewController;
+    ImagePickerPlugin* instance =[[ImagePickerPlugin alloc] initWithViewController:viewController];
+    [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    self.result=result;
+    if ([@"getGallery" isEqualToString:call.method]) {
+        TZImagePickerController *imagePickerVc = [[TZImagePickerController alloc] initWithMaxImagesCount:9 delegate:self];
+        // 是否显示可选原图按钮
+          imagePickerVc.allowPickingOriginalPhoto = YES;
+          // 是否允许显示视频
+          imagePickerVc.allowPickingVideo = YES;
+          // 是否允许显示图片
+          imagePickerVc.allowPickingImage = YES;
+
+        // You can get the photos by block, the same as by delegate.
+        // 你可以通过block或者代理，来得到用户选择的照片.
+//        [imagePickerVc setDidFinishPickingPhotosHandle:^(NSArray<UIImage *> *photos, NSArray *assets, BOOL isSelectOriginalPhoto) {
+//            NSLog(@"%@",photos);
+//            NSLog(@"%@",assets);
+//            self.result(assets);
+//        }];
+        
+//        [imagePickerVc setDidFinishPickingPhotosWithInfosHandle:^(NSArray<UIImage *> *photos,NSArray *assets,BOOL isSelectOriginalPhoto,NSArray<NSDictionary *> *infos){
+//            NSLog(@"%@",infos);
+//            for (NSDictionary *info in infos) {
+//
+//            }
+//        }];
+        [_viewController presentViewController:imagePickerVc animated:YES completion:nil];
+    }
+}
+
+- (void)imagePickerController:(TZImagePickerController *)picker didFinishPickingPhotos:(NSArray<UIImage *> *)photos sourceAssets:(NSArray *)assets isSelectOriginalPhoto:(BOOL)isSelectOriginalPhoto infos:(NSArray<NSDictionary *> *)infos{
+    
+    NSLog(@"%@",photos);
+    NSMutableArray<FlutterStandardTypedData *> *list = [NSMutableArray array];
+    for(UIImage *image in photos){
+        //NSData *imageData = UIImageJPEGRep\\(image);
+        NSData *imageData = UIImagePNGRepresentation(image);
+        NSString * str  =[[NSString alloc] initWithData:imageData encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",imageData);
+        FlutterStandardTypedData *data=[FlutterStandardTypedData typedDataWithBytes:imageData];
+        [list addObject:data];
+    }
+    
+    self.result(list);
+    
+}
+
+// 选择视频的回调
+-(void)imagePickerController:(TZImagePickerController *)picker
+       didFinishPickingVideo:(UIImage *)coverImage
+                sourceAssets:(PHAsset *)asset{
+    
+}
+    
+
+
+@end
