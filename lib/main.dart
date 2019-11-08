b8c27f2be722498e4e9f81bf72a65ca1fb8191af
lujunyu <toplu1015@163.com>
2019-11-08 14:55:34 +0800

    ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'dart:async';
+
 
 /**
  *使用平台通道调用原生代码
  */
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'service_method.dart';
 void main() => runApp(MyApp());
 
